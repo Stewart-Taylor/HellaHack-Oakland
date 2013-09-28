7bc6993c921eadfdcdaac2f160df14bd60b11240
Stewart Taylor <stewart.taylor1@gmail.com>
2013-09-28 14:55:41 -0700

    Added JS Files

--- Functions/control_panel.php
+++ Functions/control_panel.php
@@ -6,8 +6,26 @@
 
 ?>
 
-<h1>control panel</h1>
-<h1>control panel</h1>
-<h1>control panel</h1>
-v<h1>control panel</h1>
-<h1>control panel</h1>
+
+
+<div id="panel_container">
+
+<div id="mood_box" class="cp_box">
+
+<div class="cp_box_title"> Mood</div>
+</div>
+
+<div id="energy_box" class="cp_box">
+
+<div class="cp_box_title"> Energy</div>
+</div>
+
+
+<div id="speech_box"class="cp_box">
+
+<div class="cp_box_title"> Speech</div>
+</div>
+
+<button type="button" id="cp_button">Construct Query</button>
+
+</div>
