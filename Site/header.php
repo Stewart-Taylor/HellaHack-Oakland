fc954d5c2e17524632d4568d8d01a44abfd276bc
Stewart Taylor <stewart.taylor1@gmail.com>
2013-09-28 13:59:09 -0700

    Added Files

---
+++ Site/header.php
@@ -0,0 +1,39 @@
+<center><div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="/">Honey Tracker <img  src="http://honeytracker.com/assets/img/TrackerLogo.png" width="20" height="20"/></a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li ><a href="/">Home</a></li>
+			  <li ><a href="/listing.php">Items</a></li>
+			  <li ><a href="/about.php">About</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+          
+          <form name="input" action="/listing.php" method="get">
+          <div class="controls">
+              <div class="input-append">
+                <input class="span2" id="appendedInputButton" placeholder="Item Search" size="16" type="text" name="items"><button class="btn" type="submit"><i class="icon-search"></i></button>
+              </div>
+           </div>
+           </form>
+           
+        </div>
+      </div>
+ </div></center>
+ 
+ <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-18201924-5']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Honey Tracker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+	
+	</div>
+	
+  </body>
+</html>
