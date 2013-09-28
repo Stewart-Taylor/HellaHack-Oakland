5ab9892cd81d0f4960b123f6dc57a7991f855d60
Stewart Taylor <stewart.taylor1@gmail.com>
2013-09-28 16:22:03 -0700

    Added files

--- Functions/control_panel.php
+++ Functions/control_panel.php
@@ -13,11 +13,46 @@
 <div id="mood_box" class="cp_box">
 
 <div class="cp_box_title"> Mood</div>
+
+<div id="mood_icon_box">
+
+<div id="mood_happy" class="mood_emoteBox"> Happy</div>
+<div id="mood_sad" class="mood_emoteBox"> Sad</div>
+<div id="mood_angry" class="mood_emoteBox"> Angry</div>
+</div>
+
 </div>
 
 <div id="energy_box" class="cp_box">
 
 <div class="cp_box_title"> Energy</div>
+
+
+  <script>
+  $(function() {
+    // setup master volume
+    $( "#slider_energy" ).slider({
+      value: 100,
+      orientation: "horizontal",
+      range: "min",
+      animate: true
+    });
+    // setup graphic EQ
+    $( "#eq > span" ).each(function() {
+      // read initial values from markup and remove that
+      var value = parseInt( $( this ).text(), 10 );
+      $( this ).empty().slider({
+        value: value,
+        range: "min",
+        animate: true,
+        orientation: "vertical"
+      });
+    });
+  });
+  </script>
+  
+<div id="slider_energy"></div>
+
 </div>
 
 
@@ -26,6 +61,6 @@
 <div class="cp_box_title"> Speech</div>
 </div>
 
-<button type="button" id="cp_button">Construct Query</button>
+<button type="button" id="cp_button" class="construct_button">Construct Query</button>
 
 </div>
--- assets/js/control_panel.js
+++ assets/js/control_panel.js
@@ -6,14 +6,73 @@
 $(function() {
       $("#cp_button").click( function()
            {
-             test();
+             constructQuery();
            }
       );
 });
 
 
 
-function test()
+function constructQuery()
 {	
-	alert('button clicked');
+	var site = 'http://developer.echonest.com/api/v4/song/search?api_key=F7R69ENQKKOFBKMBP&format=json';
+	
+	
+	var results = '&results=' + 10;
+	
+	var mood = "happy";
+	
+	
+	
+	
+	var energy = $('#slider_energy').slider("option", "value");
+	energy = energy / 100;
+	console.log(energy);
+	
+	mood = '&mood=happy';
+	min_energy = '&min_energy=' + energy;
+	
+
+
+	var queryString = site + results + mood + min_energy;
+	
+	console.log(queryString);
+	//alert(queryString);
+	
+	callEchoNest(queryString);
 }
+
+
+function callEchoNest(website)
+{
+
+		   $.ajax({
+        url: website,
+        type: "get",
+        // callback handler that will be called on success
+        success: function(response, textStatus, jqXHR)
+		{
+		console.log(response);
+
+        },
+        // callback handler that will be called on error
+        error: function(jqXHR, textStatus, errorThrown){
+            // log the error to the console
+            console.log(
+                "The following error occured: "+
+                textStatus, errorThrown
+            );
+        },
+        // callback handler that will be called on completion
+        // which means, either on success or error
+        complete: function()
+		{
+            // enable the inputs
+         //   $inputs.removeAttr("disabled");
+        }
+    });
+
+
+}
+
+//http://developer.echonest.com/api/v4/song/search?api_key=F7R69ENQKKOFBKMBP&format=json&results=10&min_tempo=180&style=electronic&min_energy=0.8
